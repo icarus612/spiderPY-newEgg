b49496b25a08a5e2178fcc1ca09a119a9ec0534d
icarus <ellishogan95@gmail.com>
2020-09-04 23:33:42 -0400

    update

--- spider.py
+++ spider.py
@@ -11,17 +11,17 @@ for i, x in enumerate(containers):
 	title = x.find("a", {"class": "item-title"}).text.strip()
 	price = x.find("li", {"class": "price-current"}).text.strip()
 	shipping = x.find("li", {"class": "price-ship"}).text.strip()
-	brand = x.find("a", {"class": "item-brand"}).img["src"].strip()
+	brand = x.find("a", {"class": "item-brand"}).img['src'].strip()
 	deal = x.find("p", {"class": "item-promo"})
 	deal = deal.text.strip() if deal.text else ""
-	rating = x.find("a", {"class": "item-rating"})["title"] if x.find("a", {"class": "item-rating"}) else ""
-	image = x.find("a", {"class": "item-img"}).img["src"].strip()
+	rating = x.find("a", {"class": "item-rating"})['title'] if x.find("a", {"class": "item-rating"}) else ""
+	image = x.find("a", {"class": "item-img"}).img['src'].strip()
 	id_name = "".join(title.split(" "))            	
 	obj.append({"id": f"{i}",  f"{id_name}": {"title": f"{title}", "brand": f"{brand}", "price": f"{price}", "shipping": f"{shipping}", "rating": f"{rating}", "deal": f"{deal}", "image_url": f"{image}"}})
 
 data = {"Products": []}
 for i in obj:
-	data["Products"].append(i)
+	data['Products'].append(i)
 
-with open ("scraped.txt", "w") as s:
-	json.dump(data, s)
+with open("scraped.json", "w") as s:
+	json.dumps(data, s)
